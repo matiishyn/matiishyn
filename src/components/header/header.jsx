eced1828fe6e9152f83035e39492161baad42160
Ivan Matiishyn <matiyishyn.ivan@gmail.com>
2020-05-14 21:39:06 +0300

    header transition

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -2,6 +2,7 @@ import { Link } from "gatsby"
 import React from "react"
 import cx from "classnames"
 import "./style.scss"
+import _throttle from "lodash/throttle"
 
 const nav = [
   { to: "/", title: "Home" },
@@ -11,18 +12,52 @@ const nav = [
   { to: "/contact", title: "Contact" },
 ]
 
-const Header = ({ headerTransparent }) => (
-  <header
-    className={cx("header-component", { transparent: headerTransparent })}
-  >
-    <div>
-      {nav.map(el => (
-        <Link activeClassName="active" to={el.to} key={el.to}>
-          {el.title}
-        </Link>
-      ))}
-    </div>
-  </header>
-)
+class Header extends React.Component {
+  constructor(props) {
+    super(props)
+    this.ref = React.createRef()
+  }
+
+  state = {
+    isHeaderLower: false,
+  }
+
+  componentDidMount() {
+    window.addEventListener("scroll", _throttle(this.handleScroll, 100))
+  }
+
+  handleScroll = e => {
+    console.log(e, window.pageYOffset)
+    if (window.pageYOffset > 50) {
+      this.setState({ isHeaderLower: true })
+    } else {
+      this.setState({ isHeaderLower: false })
+    }
+  }
+
+  render() {
+    const { headerTransparent } = this.props
+    const { isHeaderLower } = this.state
+
+    console.log(isHeaderLower);
+    return (
+      <header
+        ref={this.ref}
+        className={cx("header-component", {
+          transparent: headerTransparent && !isHeaderLower,
+        })}
+      >
+        <div>
+          {nav.map(el => (
+            <Link activeClassName="active" to={el.to} key={el.to}>
+              {el.title}
+            </Link>
+          ))}
+        </div>
+      </header>
+    )
+  }
+}
+
 
 export default Header
