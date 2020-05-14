adaa913ed0251dbf02bfed7d30b38d117578b893
Ivan Matiishyn <matiyishyn.ivan@gmail.com>
2020-05-14 12:14:26 +0300

    typography

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,16 +2,23 @@ module.exports = {
   siteMetadata: {
     title: `Ivan Matiishyn | JavaScript Developer`,
     description: `Ivan Matiishyn - Full Stack JavaScript Developer`,
-    author: `ivan.matiishyn@gmail.com`,
+    author: `ivan.matiishyn@gmail.com`
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`
+      }
+    },
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sass`, // https://github.com/gatsbyjs/gatsby/tree/master/packages/gatsby-plugin-sass
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images`,
-      },
+        path: `${__dirname}/src/images`
+      }
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
@@ -24,11 +31,11 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
+        icon: `src/images/favicon.png` // This path is relative to the root of the site.
+      }
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
-  ],
-}
+  ]
+};
--- src/components/header.js
+++ src/components/header.js
@@ -1,33 +1,31 @@
-import { Link } from "gatsby"
-import React from "react"
+import { Link } from "gatsby";
+import React from "react";
 
 const Header = ({ siteTitle }) => (
   <header
     style={{
       background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
+      marginBottom: `1.45rem`
     }}
   >
     <div
       style={{
         margin: `0 auto`,
         maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
+        padding: `1.45rem 1.0875rem`
       }}
     >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+      <Link
+        to="/"
+        style={{
+          color: `white`,
+          textDecoration: `none`
+        }}
+      >
+        {siteTitle}
+      </Link>
     </div>
   </header>
-)
+);
 
-export default Header
+export default Header;
