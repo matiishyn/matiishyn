4c5ff7e7012991b218e2538947cf03ba42aa8ff9
Ivan Matiishyn <matiyishyn.ivan@gmail.com>
2020-05-15 09:38:03 +0300

    mobile menu component

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -3,6 +3,7 @@ import React from "react"
 import cx from "classnames"
 import "./style.scss"
 import _throttle from "lodash/throttle"
+import { MobileMenu } from "./mobile-menu"
 
 const nav = [
   { to: "/", title: "Home" },
@@ -56,86 +57,11 @@ class Header extends React.Component {
           ))}
         </nav>
 
-        {/* Mobile Menu */}
-        <div id="mobile-menu-wrapper">
-          <nav
-            id="nav"
-            className={cx("nav", { "nav--open": isMobileMenuOpen })}
-            role="navigation"
-          >
-            <ul
-              className="nav__menu"
-              id="menu"
-              tabIndex="-1"
-              aria-label="main navigation"
-              hidden
-            >
-              {nav.map(el => (
-                <li className="nav__item" key={el.to}>
-                  <Link
-                    activeClassName="active"
-                    to={el.to}
-                    className="nav__link"
-                  >
-                    {el.title}
-                  </Link>
-                </li>
-              ))}
-            </ul>
-
-            <a
-              // href="#nav"
-              className="nav__toggle"
-              onClick={this.toggleMobileMenu}
-              role="button"
-              aria-expanded="false"
-              aria-controls="menu"
-            >
-              <svg
-                className="menuicon"
-                xmlns="http://www.w3.org/2000/svg"
-                width="50"
-                height="50"
-                viewBox="0 0 50 50"
-              >
-                <title>Toggle Menu</title>
-                <g>
-                  <line
-                    className="menuicon__bar"
-                    x1="13"
-                    y1="16.5"
-                    x2="37"
-                    y2="16.5"
-                  />
-                  <line
-                    className="menuicon__bar"
-                    x1="13"
-                    y1="24.5"
-                    x2="37"
-                    y2="24.5"
-                  />
-                  <line
-                    className="menuicon__bar"
-                    x1="13"
-                    y1="24.5"
-                    x2="37"
-                    y2="24.5"
-                  />
-                  <line
-                    className="menuicon__bar"
-                    x1="13"
-                    y1="32.5"
-                    x2="37"
-                    y2="32.5"
-                  />
-                  <circle className="menuicon__circle" r="23" cx="25" cy="25" />
-                </g>
-              </svg>
-            </a>
-
-            <div className="splash" />
-          </nav>
-        </div>
+        <MobileMenu
+          isOpen={isMobileMenuOpen}
+          menuItems={nav}
+          toggle={this.toggleMobileMenu}
+        />
       </header>
     )
   }
