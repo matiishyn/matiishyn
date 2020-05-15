170f5371a35e5df77f566bb902abe4b805a5b3b0
Ivan Matiishyn <matiyishyn.ivan@gmail.com>
2020-05-15 16:19:58 +0300

    services

---
+++ src/components/services/data.js
@@ -0,0 +1,35 @@
+export const services = [
+  {
+    title: "Front End Developer",
+    description: `My primary skill is front end development<br>
+                  I’ve been doing it for 8 years.
+                  Took part in different project phases, 
+                  created projects from scratch, 
+                  established front-end architecture and successfully released them.
+    `,
+  },
+  {
+    title: "Full Stack Developer",
+    description: `My secondary skill is Node.js<br> 
+                  I have experience developing 
+                  Node.js Apps with Express.js using 
+                  MongoDB and SQL, Serverless Web Apps, 
+                  ServerSideRendered Web Apps
+  `,
+  },
+  {
+    title: "Team Leader / Manager",
+    description: `- Team Leader of a team up to 10 people<br>
+                  - Development Community leader<br> 
+                  - Competence manager<br>
+                  - Software Engineering manager<br>
+                  - Interviewer<br>
+                  ` },
+  {
+    title: "Mentor / Teacher / Speaker",
+    description: `- Mentor<br>
+                  - Speaker<br>
+                  - Course Creator<br>
+    `
+  },
+]
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,12 +3,14 @@ import Layout from "../components/layout/layout"
 import Banner from "../components/banner/banner"
 import SEO from "../components/seo"
 import { About } from "../components/about/about"
+import { Services } from "../components/services/services"
 
 const IndexPage = () => (
   <Layout headerTransparent>
     <SEO title="Home" />
     <Banner />
     <About />
+    <Services />
   </Layout>
 )
 
