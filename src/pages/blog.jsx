7f68d32d8fa74ee920f5bca0d00634940c4c0727
Ivan Matiishyn <matiyishyn.ivan@gmail.com>
2020-05-19 16:43:12 +0300

    blog text update

--- src/pages/blog.jsx
+++ src/pages/blog.jsx
@@ -7,7 +7,14 @@ const BlogPage = () => (
     <SEO title="Blog" />
     <div className="container">
       <h1 className="section-header">blog</h1>
-      <p>this section is not ready yet...</p>
+      <p>
+        Having spent some time configuring this part of my website
+        I came to the decision that I actually don't need it. <br/>
+        Even if I had a blog I don't think I would write something <br/>
+        Last but not least, I prefer to communicate with other people
+        rather than a code :) <br/>
+        So, I'll just keep this page as it is now
+      </p>
     </div>
   </Layout>
 )
